e50f3f9d2bee53d28664ca5f7d9ff2af176067c6
Rita Mwangi <ritahmwangi@gmail.com>
2023-01-19 11:01:29 -0800

    changes made

--- pall.c
+++ pall.c
@@ -1,23 +1,23 @@
 #include "monty.h"
 
 /*
- * pall - Entry point
+ * op_pall - Entry point
  *
  * Description: Prints out all the values of the stack
+ *
  * @stack: Address of the stack to print
  * @line_numebr: Line number of the opcode being executed
  */
 
-void pall(stack_t **stack, unsigned int line_number)
+void op_pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp = *stack;
-	int counter = 0;
-	(void) line_number;
+	stack_t *arr = *stack;
 
-	while (tmp != NULL)
+	while (arr)
 	{
-		printf("%d\n", tmp->n);
-		counter++;
-		tmp = tmp->next;
+		printf("%d\n", arr->n);
+		arr = arr->next;
 	}
+
+	(void)line_number;
 }
--- push.c
+++ push.c
@@ -1,48 +1,41 @@
 #include "monty.h"
 
 /**
- * push - Entry point
+ * op_push - Entry point
  *
  * Description: Adds an element to the top of a stack
  * @stack: Address of the stack element to be added
  * @line_number: The line of the opcode being executed
  */
 
-void push(stack_t **stack, unsigned int line_number)
+void op_push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *new_node = malloc(sizeof(stack_t));
-	int m = 0;
+	int num;
+	stack_t *new;
 
-	for (m = 0; push_data[m] != '\0'; m++)
+	if (is_valid_int(op.arg) == 0)
 	{
-		if (isdigit(push_data[m]) == 0)
-		{
-			fprintf(stderr, "L%d: usage: push integer\n",
-					line_number);
-			free_stack(*stack);
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "ERROR: malloc failed\n");
+		fprint(stderr, "L%u: usage: push integer\n", line_number);
+		free_op();
 		exit(EXIT_FAILURE);
 	}
-	new_node->n = atoi(push_data);
-	new_node->prev = NULL;
-	new_node->next = NULL;
+	num = atoi(op.arg);
 
-	/* In case head is an empty list */
-	if (*stack == NULL)
+	/* add num to beginning of stack */
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
 	{
-		*stack = new_node;
+		fprintf(stderr, "Error: malloc failed\n");
+		free_op();
+		exit(EXIT_FAILURE);
 	}
-	/* If head is not an empty list */
-	else
-	{
-		new_node->next = *stack;
-		(*stack)->prev = new_node;
-		*stack = new_node;
-	}
+
+	new->n = num;
+	new->prev = NULL;
+	new->next = *stack;
+
+	if (*stack)
+		(*stack)->prev = new;
+
+	*stack = new;
 }
