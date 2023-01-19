b4e57c02fc4d349246a3a9a71ac62f3e42639601
Rita Mwangi <ritahmwangi@gmail.com>
2023-01-19 11:15:05 -0800

    Changes

--- pall.c
+++ pall.c
@@ -1,12 +1,12 @@
 #include "monty.h"
 
-/*
+/**
  * op_pall - Entry point
  *
  * Description: Prints out all the values of the stack
  *
  * @stack: Address of the stack to print
- * @line_numebr: Line number of the opcode being executed
+ * @line_number: Line number of the opcode being executed
  */
 
 void op_pall(stack_t **stack, unsigned int line_number)
